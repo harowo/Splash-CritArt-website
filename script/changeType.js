f49917eafaf914d8d2faaedb846d253868a86364
Eric <wang.yaohui@dhs.sg>
2020-09-26 01:56:16 +0800

    first commit

---
+++ script/changeType.js
@@ -0,0 +1,8 @@
+var type = [ 'Genre', 'Colour', 'Vibrancy', 'Temperature' ];
+var typeId = 0;
+
+window.setInterval(function () {
+    $('#type').html(type[typeId]);
+    typeId = typeId + 1;
+    if(typeId >= type.length) { typeId = 0; }
+}, 3000);
